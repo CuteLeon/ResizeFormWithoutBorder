3c39ba4a85ebf126664b48abdd3c9a6f4784bd8f
Leon <Leon.ID@QQ.COM>
2017-09-07 16:51:09 +0800

    第一次提交

---
+++ ResizeFormWithoutBorderViaMouse/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResizeFormWithoutBorderViaMouse
+{
+    public partial class MainForm : Form
+    {
+        #region 消息常量
+        const int WM_NCHITTEST = 0x0084;
+        const int HT_LEFT = 10;
+        const int HT_RIGHT = 11;
+        const int HT_TOP = 12;
+        const int HT_TOPLEFT = 13;
+        const int HT_TOPRIGHT = 14;
+        const int HT_BOTTOM = 15;
+        const int HT_BOTTOMLEFT = 16;
+        const int HT_BOTTOMRIGHT = 17;
+        const int HT_CAPTION = 2;
+        #endregion
+
+        #region 复写消息处理函数 WndProc
+        protected override void WndProc(ref Message Msg)
+        {
+            if (Msg.Msg == WM_NCHITTEST)
+            {
+                //获取鼠标位置
+                int nPosX = (Msg.LParam.ToInt32() & 65535);
+                int nPosY = (Msg.LParam.ToInt32() >> 16);
+                //右下角
+                if (nPosX >= this.Right - 6 && nPosY >= this.Bottom - 6)
+                {
+                    Msg.Result = new IntPtr(HT_BOTTOMRIGHT);
+                    return;
+                }
+                //左上角
+                else if (nPosX <= this.Left + 6 && nPosY <= this.Top + 6)
+                {
+                    Msg.Result = new IntPtr(HT_TOPLEFT);
+                    return;
+                }
+                //左下角
+                else if (nPosX <= this.Left + 6 && nPosY >= this.Bottom - 6)
+                {
+                    Msg.Result = new IntPtr(HT_BOTTOMLEFT);
+                    return;
+                }
+                //右上角
+                else if (nPosX >= this.Right - 6 && nPosY <= this.Top + 6)
+                {
+                    Msg.Result = new IntPtr(HT_TOPRIGHT);
+                    return;
+                }
+                else if (nPosX >= this.Right - 2)
+                {
+                    Msg.Result = new IntPtr(HT_RIGHT);
+                    return;
+                }
+                else if (nPosY >= this.Bottom - 2)
+                {
+                    Msg.Result = new IntPtr(HT_BOTTOM);
+                    return;
+                }
+                else if (nPosX <= this.Left + 2)
+                {
+                    Msg.Result = new IntPtr(HT_LEFT);
+                    return;
+                }
+                else if (nPosY <= this.Top + 2)
+                {
+                    Msg.Result = new IntPtr(HT_TOP);
+                    return;
+                }
+                else
+                {
+                    Msg.Result = new IntPtr(HT_CAPTION);
+                    return;
+                }
+            }
+            base.WndProc(ref Msg);
+        }
+        #endregion
+
+        #region 为窗体增加圆角效果
+        public void SetWindowRegion()
+        {
+            System.Drawing.Drawing2D.GraphicsPath FormPath;
+            FormPath = new System.Drawing.Drawing2D.GraphicsPath();
+            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+            FormPath = GetRoundedRectPath(rect, 10);
+            this.Region = new Region(FormPath);
+
+        }
+        private System.Drawing.Drawing2D.GraphicsPath GetRoundedRectPath(Rectangle rect, int radius)
+        {
+            int diameter = radius;
+            Rectangle arcRect = new Rectangle(rect.Location, new Size(diameter, diameter));
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // 左上角
+            path.AddArc(arcRect, 180, 90);
+
+            // 右上角
+            arcRect.X = rect.Right - diameter;
+            path.AddArc(arcRect, 270, 90);
+
+            // 右下角
+            arcRect.Y = rect.Bottom - diameter;
+            path.AddArc(arcRect, 0, 90);
+
+            // 左下角
+            arcRect.X = rect.Left;
+            path.AddArc(arcRect, 90, 90);
+            path.CloseFigure();//闭合曲线
+            return path;
+        }
+        #endregion
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            SetWindowRegion();
+            this.Resize += new EventHandler((s,e) =>{SetWindowRegion();});
+        }
+    }
+}
